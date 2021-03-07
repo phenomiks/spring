9fd6ab0dba1bd1d925a814b963df1e2bbdedfa89
phenomiks <phenomikspc@gmail.com>
2021-03-07 13:45:40 +0300

    Revert "lesson3_homework"
    This reverts commit a5ccdef85e7f3c788ef06b9ca6a8154c0bc8b28c.

--- less3_SpringMVC/src/main/java/ru/geekbrains/spring/mvc/configuration/WebInitializer.java
+++
@@ -1,4 +0,0 @@
-package ru.geekbrains.spring.mvc.configuration;
-
-public class WebInitializer {
-}
--- less3_SpringMVC/src/main/java/ru/geekbrains/spring/mvc/contollers/RepositoryController.java
+++
@@ -1,4 +0,0 @@
-package ru.geekbrains.spring.mvc.contollers;
-
-public class RepositoryController {
-}
